526dee0e0a30b866bbd04f72fce247b1e2754373
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-13 17:39:24 +0000

    File System
    Learnt a lot on files, creating, editing, deleting files and so on

--- 02TUT/index.js
+++ 02TUT/index.js
@@ -1,12 +1,31 @@
 const fsPromises = require('fs').promises;
 const path = require('path');
 
+const fileOps = async () =>  {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
+        console.log(data);
+        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
+
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you.');
+        await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8')
+        console.log(newData);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+fileOps();
+
+/*
 fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 })
 
-/*
 fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (err) => {
     if (err) throw err;
     console.log('Write complete');
@@ -20,7 +39,7 @@ fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you.', (e
             console.log('Rename complete...');
         })
     })
-}) */
+})
 
 fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing text.', (err) => {
     if (err) throw err;
@@ -30,4 +49,4 @@ fs.appendFile(path.join(__dirname, 'files', 'test.txt'), 'Testing text.', (err) 
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`);
     process.exit(1);
-})
+}) */
