31835777e9a99a55a459d0abe361c6442fdb5aa6
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-28 15:04:36 +0000

    Serving a file version 1
    This is a lot to write so this is not a version to use for your paths

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -5,7 +5,7 @@ const fsPromises = require("fs").promises;
 
 const logEvents = require('./logEvents');
 const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
+class Emitter extends EventEmitter { };
 // initialize object
 const myEmitter = new Emitter();
 
@@ -13,6 +13,16 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
+
+    let filePath;
+    if (req.url === '/' || req.url === 'index.html') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        filePath = path.join(__dirname, 'views', 'index.html');
+        fs.readFile(path, 'utf8', (err, data) => {
+            res.end(data);
+        });
+    }
 });
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
