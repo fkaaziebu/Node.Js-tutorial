f180796ff8c271fc0ea6a66862c83d7cb5a641ce
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-29 09:34:22 +0000

    Express Chain route handling
    Used next to switch to other routes to another route

--- 06TUT/server.js
+++ 06TUT/server.js
@@ -16,8 +16,35 @@ app.get('/old-page(.html)?', (req, res) => {
     res.redirect(301, '/new-page.html'); // 302 status code by default
 });
 
+// Route handlers
+app.get('hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.setDefaultEncoding('Hello World!');
+});
+
+// Chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished')
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
 app.get('/*', (req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html')); //200 status code by default
-})
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
